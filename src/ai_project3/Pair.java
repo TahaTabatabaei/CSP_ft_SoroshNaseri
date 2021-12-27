f12dee66b926356aae3ccd89c3a0ebe5368827a9
TahaTabatabaei <sied.taha@gmail.com>
2021-12-27 21:33:44 +0330

    arc types

---
+++ src/ai_project3/Pair.java
@@ -0,0 +1,13 @@
+package ai_project3;
+
+public class Pair {
+
+    Pair(){}
+
+    Pair(Variable varSource,Variable varTarget ){
+        var1 = varSource;
+        var2 = varTarget;
+    }
+    Variable var1;
+    Variable var2;
+}
